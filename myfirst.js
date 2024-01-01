b91cb7b1da622d13debcf97de219c47405b0021f
momo <mhamedbattioui@gmail.com>
2024-01-01 16:12:01 -0500

    nodejs

--- myfirst.js
+++ myfirst.js
@@ -1,9 +1,14 @@
 
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end('Hello World!');
+	fs.readFile('html/main.html', function(err, data) {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data);
+		return res.end();
+	});
 }).listen(8080);
 
 
