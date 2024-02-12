312dbe4836e0009579863194a083377dc5d24a01
momo <mhamedbattioui@gmail.com>
2024-02-12 08:25:01 -0500

    fonctionnalite

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # main-website
+
+pas de pub
+
+il peut y avoir des articles
+
+un site comme celui que je voudrais visiter
+
+il y aura une plateforme de discussion si la personne se connecte
+
+des liens pour telecharger des documents
+
--- app.js
+++ app.js
@@ -4,7 +4,13 @@ var url = require('url');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-	fs.readFile('html/main.html', function(err, data) {
+	var q = url.parse(req.url, true);
+	var filename = "." + q.pathname;
+	fs.readFile(filename, function(err, data) {
+		if (err) {
+			res.writeHead(404, {'Content-Type': 'html'});
+			return res.end("404 Not Found");
+		}
 		res.writeHead(200, {'Content-Type': 'text/html'});
 		res.write(data);
 		return res.end();
